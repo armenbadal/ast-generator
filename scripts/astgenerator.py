34379db4954fbf7f3b50f73df8ba0ef3128f3051
Արմեն Բադալյան <armenbadal@gmail.com>
2026-03-11 14:32:42 +0400

    Սկրիպտի առաջին մասը

---
+++ scripts/astgenerator.py
@@ -0,0 +1,163 @@
+
+# Թոքեններ
+IDENTIFIER = 'IDENTIFIER'
+LEFT_BRACKET = '['
+RIGHT_BRACKET = ']'
+EXCLAMATION = '!'
+COLON = ':'
+COMMA = ','
+VERBATIM = '@'
+NEW_LINE = '↵'
+SPACE = '␣'
+EOF = 'EOF'
+
+class Lexeme:
+    def __init__(self, token, value):
+        self.token = token
+        self.value = value
+
+class Scanner:
+    def __init__(self, source):
+        self.source = source
+        self.position = 0
+        self.length = len(source)
+
+    def next_lexeme(self):
+        if self.position >= self.length:
+            return Lexeme(EOF, None)
+
+        current_char = self.source[self.position]
+
+        if current_char.isalpha():
+            start_pos = self.position
+            while self.position < self.length and self.source[self.position].isalnum():
+                self.position += 1
+            return Lexeme(IDENTIFIER, self.source[start_pos:self.position])
+        if current_char == '@':
+            start_pos = self.position
+            while self.position < self.length and self.source[self.position] != '\n':
+                self.position += 1
+            return Lexeme(VERBATIM, self.source[start_pos+1:self.position])
+        elif current_char == '[':
+            self.position += 1
+            return Lexeme(LEFT_BRACKET, '[')
+        elif current_char == ']':
+            self.position += 1
+            return Lexeme(RIGHT_BRACKET, ']')
+        elif current_char == '!':
+            self.position += 1
+            return Lexeme(EXCLAMATION, '!')
+        elif current_char == ':':
+            self.position += 1
+            return Lexeme(COLON, ':')
+        elif current_char == ',':
+            self.position += 1
+            return Lexeme(COMMA, ',')
+        elif current_char == '\n':
+            self.position += 1
+            return Lexeme(NEW_LINE, '\n')
+        elif current_char.isspace():
+            self.position += 1
+            return Lexeme(SPACE, ' ')
+        else:
+            raise ValueError(f'Unexpected character: {current_char}')
+
+
+class Parser:
+    def __init__(self, scanner):
+        self.scanner = scanner
+        self.lookahead = self.scanner.next_lexeme()
+
+    def match(self, token):
+        if self.lookahead.token == token:
+            value = self.lookahead.value
+            self.lookahead = self.scanner.next_lexeme()
+            return value
+        
+        raise ValueError(f'Expected token {token}, got {self.lookahead.token}.')
+
+    def has(self, token):
+        return self.lookahead.token == token
+
+    def parse(self):
+        verbatims = []
+        if self.has(VERBATIM):
+            verbatim = self.match(VERBATIM)
+            verbatims.append(verbatim)
+            self.parse_new_lines()
+
+        definitions = []
+        while self.lookahead.token != EOF:
+            definition = self.parse_definition()
+            definitions.append(definition)
+
+        return (verbatims, definitions)
+
+    def parse_definition(self):
+        indent = self.parse_indent()
+        name = self.match(IDENTIFIER)
+        slots = None
+        if self.has(LEFT_BRACKET):
+            self.match(LEFT_BRACKET)
+            slots = self.parse_slots()
+            self.match(RIGHT_BRACKET)
+        self.parse_new_lines()
+        return (indent, name, slots)
+
+    def parse_indent(self):
+        count = 0
+        while self.lookahead.token == SPACE:
+            count += 1
+            self.match(SPACE)
+        return count
+
+    def parse_slots(self):
+        if self.has(RIGHT_BRACKET):
+            return []
+        
+        slots = []
+        slots.append(self.parse_slot())
+        while self.lookahead.token == COMMA:
+            self.match(COMMA)
+            slots.append(self.parse_slot())
+        return slots
+
+    def parse_slot(self):
+        name = self.match(IDENTIFIER)
+        self.match(COLON)
+        type = self.match(IDENTIFIER)
+        return (name, type)
+
+    def parse_new_lines(self):
+        while self.lookahead.token == NEW_LINE:
+            self.match(NEW_LINE)
+
+
+
+class Generator:
+    def __init__(self, ast, directory):
+        self.ast = ast
+        self.directory = directory
+
+
+class JavaGenerator(Generator):
+    def __init__(self, ast, directory):
+        super().__init__(ast, directory)
+
+    def generate(self):
+        verbatims, definitions = self.ast
+        preamble = '\n'.join(verbatims)
+
+
+    def generate_one_class(self, preamble, definition):
+        pass
+
+
+if __name__ == "__main__":
+    with open('examples/ex01.ast', 'r') as f:
+        source = f.read()
+    scanner = Scanner(source)
+    parser = Parser(scanner)
+    result = parser.parse()
+    print(result)
+
